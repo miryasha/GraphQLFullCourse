aab6118c48b5d858580cbf2e0d22740bd113ce8d
cat-coder <cat-coder>
2022-04-27 21:46:08 -0500

    react added

--- server/app.js
+++ server/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const graphqlHTTP = require("express-graphql");
+const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect('mongodb+srv://root:rootroot@qraph.iffsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
+
+// bind express with graphql
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
-
 app.listen(4000, () => {
-    console.log("now listening for request on port 4000")
+    console.log('now listening for requests on port 4000');
 });
